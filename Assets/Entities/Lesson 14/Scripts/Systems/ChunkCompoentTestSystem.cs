b854fc8cb8f48b8ef0251bf4d1cef9760e76ecfe
WoBok <darwin.vinci.wkc@gmail.com>
2024-08-05 21:11:59 +0800

    Lesson 14

--- Assets/Entities/Lesson 14/Scripts/Systems/ChunkCompoentTestSystem.cs
+++ Assets/Entities/Lesson 14/Scripts/Systems/ChunkCompoentTestSystem.cs
@@ -10,9 +10,25 @@ namespace Entity_Lesson14
         public void OnCreate(ref SystemState state)
         {
             var entity1 = state.EntityManager.CreateEntity();
-            state.EntityManager.AddComponentData<GeneralComponent>(entity1, new GeneralComponent { num = 1 });
+            state.EntityManager.AddComponentData(entity1, new GeneralComponent { num = 1 });
             state.EntityManager.AddChunkComponentData<ChunkComponentA>(entity1);
+
+            var entity2 = state.EntityManager.CreateEntity();
+            state.EntityManager.AddComponentData(entity2, new GeneralComponent { num = 2 });
+            state.EntityManager.AddChunkComponentData<ChunkComponentA>(entity2);
+            state.EntityManager.AddChunkComponentData<ChunkComponentB>(entity2);
+
+            ArchetypeChunk chunk = state.EntityManager.GetChunk(entity1);
+            state.EntityManager.SetChunkComponentData(chunk, new ChunkComponentA { numA = 2 });
+
+            var entity3 = state.EntityManager.CreateEntity();
+            state.EntityManager.AddChunkComponentData<ChunkComponentA>(entity3);
+            state.EntityManager.AddChunkComponentData<ChunkComponentB>(entity3);
+
             state.Enabled = false;
         }
     }
 }
+
+//var entity4 = state.EntityManager.CreateEntity();
+//state.EntityManager.AddChunkComponentData<ChunkComponentAB>(entity4);
