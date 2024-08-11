a008fd17c631ade65375feeedfd3c3036e33afe7
WoBok <darwin.vinci.wkc@gmail.com>
2024-08-11 21:23:27 +0800

    DOTS Shader

--- Assets/Entities Graphics/Material Overrides/Scripts/Components/MaterialColorComponent.cs
+++ Assets/Entities Graphics/Material Overrides/Scripts/Components/MaterialColorComponent.cs
@@ -4,7 +4,8 @@ using Unity.Rendering;
 
 namespace MaterialOverrides
 {
-    [MaterialProperty("_ColorDOTS")]
+    //[MaterialProperty("_ColorDOTS")]
+    [MaterialProperty("_BaseColor")]
     public struct MaterialColorComponent : IComponentData
     {
         public float4 color;
